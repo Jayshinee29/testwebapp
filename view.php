9e9d83b5166aa01959f137faadeed729ac88320b
Jayshinee29 <66692068+Jayshinee29@users.noreply.github.com>
2020-06-12 20:40:26 +0800

    Update view.php

--- view.php
+++ view.php
@@ -1,6 +1,6 @@
 <?php
 //connection string
-$connectionInfo = array("UID" => "jayshinee", "pwd" => "queenjay123456@", "Database" => "ddacdbtp046742", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "jayshinee", "pwd" => "queenjay123@", "Database" => "ddacdbtp046742", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:ddactp046742.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
